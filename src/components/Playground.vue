76fb388e7d0604e199b09397c6f2b2cff6000f08
Andrey <eugene@totally.group>
2022-06-14 22:35:55 +0300

    Added TeeterTotter.vue

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="playground playground-container"></div>
+  <div class="playground playground-container">
+    <teeter-totter class="playground__teeter"/>
+  </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
+import TeeterTotter from '@/components/TeeterTotter.vue'
 
 @Component({
   components: {
+    TeeterTotter
   },
 })
 
@@ -14,12 +18,20 @@ export default class Playground extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+.playground {
+  &__teeter {
+    position: absolute;
+    bottom: 10%;
+  }
+}
+
 // TODO: get header height from variables
 .playground-container {
+  position: relative;
   max-width: 800px;
   min-width: 800px;
   width: 800px;
   margin: auto;
-  height: calc(100% - 35px);
+  height: calc(100vh - 35px);
 }
 </style>
---
+++ src/components/TeeterTotter.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="teeter">
+  <div class="teeter__hr" />
+  <div class="teeter__triangle" />
+</div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({
+  components: {
+  },
+})
+export default class TeeterTotter extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.teeter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+
+  // TODO: Add func to adapt width to px 1m = 60px
+  &__hr {
+    width: 600px;
+    height: 1px;
+    background-color: red;
+  }
+
+  &__triangle {
+    width: 100px;
+    height: 100px;
+    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+    background-color: #c7c7c7;
+  }
+}
+</style>
