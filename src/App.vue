b760c5434402e1d9ad2ebde1af8b5f0a89cfbdbc
Andrey <eugene@totally.group>
2022-06-14 20:07:20 +0300

    Added Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    Hello
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
 
 @Component({
   components: {
+    Header
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "scss/index.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+<header class="header">
+  <div class="container">
+    <button>Play</button>
+  </div>
+</header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 35px;
+  background-color: #fafafa;
+}
+</style>
