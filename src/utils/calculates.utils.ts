d35779c46123f3bcaeb49f113b4fe1d171c8017f
Andrey <eugene@totally.group>
2022-06-19 19:57:15 +0300

    Added comments to calculates.utils.ts

--- src/utils/calculates.utils.ts
+++ src/utils/calculates.utils.ts
@@ -1,11 +1,26 @@
+/**
+ * Return random number in a range from min to max
+ * @param min
+ * @param max
+ */
 export function getRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+/**
+ * Return random coorfinate in a range from min to max
+ * @param minPos
+ * @param maxPos
+ */
 export function getRandomPosition(minPos: number, maxPos: number): number {
   return getRandomNumber(minPos, maxPos);
 }
 
+/**
+ * Calculate momentum
+ * @param shoulder
+ * @param weight
+ */
 export function getMomentum(shoulder: number, weight: number): number {
   return shoulder * weight;
 }
