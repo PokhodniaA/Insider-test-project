9b2eb514234ef4c24b26fa9658a4594246d521a5
Andrey <eugene@totally.group>
2022-06-19 10:23:40 +0300

    Fixed rotate formula

--- src/classes/TeeterTotter.ts
+++ src/classes/TeeterTotter.ts
@@ -2,7 +2,7 @@ import store from "@/store";
 import {FIELD_WIDTH} from "@/store/getters.const";
 
 const TEETER_TOTTER_WIDTH = 10;
-const MAX_ROTATE_ANGLE = 30;
+const MAX_ROTATE_ANGLE = 14;
 
 export default class TeeterTotterClass {
     private teeterTotterWidth: number = TEETER_TOTTER_WIDTH;
@@ -23,20 +23,23 @@ export default class TeeterTotterClass {
         if (this.momentum === 0) {
             return 0;
         }
-        this.rotateAngle = Math.round(this.maxRotateAngle / this.momentum);
+        this.rotateAngle = Math.round((this.maxRotateAngle * this.momentum) / 20);
     }
 
     public getMomentShoulder(pos: number): number {
         const widthInPx = store.getters[FIELD_WIDTH] / 2;
-        return(pos * (this.teeterTotterWidth / 2)) / widthInPx
+        return(pos * (this.teeterTotterWidth / 2)) / widthInPx;
     }
 
     public setMomentum(leftSide: number, rightSide: number) {
+        console.log(leftSide, 'left-momentum')
+        console.log(rightSide, 'right-momentum')
         if (leftSide > rightSide) {
             this.momentum += rightSide - leftSide;
         } else {
             this.momentum -= leftSide - rightSide;
         }
+        console.log(this.momentum, 'setted momentum')
 
         this.updateRotateAngle()
     }
